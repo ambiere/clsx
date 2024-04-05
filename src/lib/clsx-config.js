194227966b5cb9321f5313e2210f4cca28703bc2
zhid0399123 <zhid0399123@gmail.com>
2024-04-05 17:38:14 +0300

    module not found returns empty {}

---
+++ src/lib/clsx-config.js
@@ -0,0 +1,20 @@
+import path from "path"
+import projectRootDir from "@ambiere/project-root"
+
+export default async function nextClsxConfig() {
+  try {
+    const rootDir = projectRootDir()
+    if (rootDir) {
+      const pathToConfigs = path.resolve(path.join(rootDir, "clsx.config.mjs"))
+      const configs = await import(pathToConfigs)
+      if (configs) return configs
+    }
+  } catch (error) {
+    if (error.code === "ERR_MODULE_NOT_FOUND") {
+      return {}
+    }
+    console.log(error)
+    return {}
+  }
+}
+
