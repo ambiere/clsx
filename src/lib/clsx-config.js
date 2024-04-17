781b063648abe2a40b16200911dfabe899d8aab9
zhid0399123 <zhid0399123@gmail.com>
2024-04-17 22:04:40 +0300

    read next-clsx configs sync
    use to-object deps, which parse string objects sync

--- src/lib/clsx-config.js
+++ src/lib/clsx-config.js
@@ -1,21 +1,26 @@
 import path from "path"
+import { readFileSync } from "fs"
+import toObject from "@ambiere/to-object"
 import projectRootDir from "@ambiere/project-root"
 
-export default async function clsxConfig() {
+/**
+ * next-clsx configurations
+ * @returns {{} | import("../types").NextClsxConfigs}
+ * */
+export default function nextClsxConfigs() {
   try {
     const rootDir = projectRootDir()
     if (rootDir) {
       const pathToConfigs = path.resolve(path.join(rootDir, "clsx.config.mjs"))
-      const configs = await import(pathToConfigs)
-      if (configs.default) return configs.default
+      let configs = readFileSync(pathToConfigs, "utf8")
+      if (configs) return toObject(configs)
+      return {}
     }
   } catch (error) {
     if (error.code === "ERR_MODULE_NOT_FOUND") {
+      console.error("[Error]: next-clsx configurations file not found")
       return {}
     }
     console.log(error)
-    return {}
   }
 }
-
-
